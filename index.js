5079c9ad62a17686175004fafb33b93bc405f4ff
Rico <ricosantangelo@gmail.com>
2023-11-21 13:52:17 -0800

    Updated index.js and generatedMarkdown.js

--- index.js
+++ index.js
@@ -1,13 +1,109 @@
-// TODO: Include packages needed for this application
+// Packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of your project:'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the installation instructions?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How do you use this application?'
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'How can others contribute to your project?'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'What are the test instructions?'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license for your project:',
+        choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3', 'None']
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub Username:'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address:'
+    }
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+        err ? console.log(err) : console.log('Successfully created README.md!')
+    );
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+// Function to generate markdown for README
+function generateMarkdown(response) {
+    // Here you'll format the README based on the user responses
+    // Placeholder for README content
+    return `# ${response.title}
+    
+## Description
+${response.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [License](#license)
+- [Questions](#questions)
+
+## Installation
+${response.installation}
+
+## Usage
+${response.usage}
+
+## Contributing
+${response.contribution}
+
+## Tests
+${response.tests}
+
+## License
+${response.license === 'None' ? 'This project is not licensed.' : `This project is licensed under the ${response.license} license.`}
+
+## Questions
+For any questions, please contact me at [${response.email}](mailto:${response.email}).
+GitHub: [${response.github}](https://github.com/${response.github})
+`;
+}
+
+// Function to initialize app
+function init() {
+    inquirer.prompt(questions).then((response) => {
+        const READMEContent = generateMarkdown(response);
+        writeToFile('README.md', READMEContent);
+    });
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,90 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// Function to return a license badge based on which license is passed in
+function renderLicenseBadge(license) {
+  if (!license || license === 'None') return '';
+  let badge = '';
+  switch (license) {
+      case 'MIT':
+          badge = '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)';
+          break;
+      case 'Apache 2.0':
+          badge = '![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)';
+          break;
+      case 'GPL 3.0':
+          badge = '![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)';
+          break;
+      case 'BSD 3':
+          badge = '![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)';
+          break;
+      default:
+          // Default case can be an empty string or a generic badge
+          badge = '';
+  }
+  return badge;
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// Function to return the license link
+function renderLicenseLink(license) {
+  if (!license || license === 'None') return '';
+  let link = '';
+  switch (license) {
+      case 'MIT':
+          link = 'https://opensource.org/licenses/MIT';
+          break;
+      case 'Apache 2.0':
+          link = 'https://opensource.org/licenses/Apache-2.0';
+          break;
+      case 'GPL 3.0':
+          link = 'https://www.gnu.org/licenses/gpl-3.0';
+          break;
+      case 'BSD 3':
+          link = 'https://opensource.org/licenses/BSD-3-Clause';
+          break;
+      default:
+          link = '';
+  }
+  return link;
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// Function to return the license section of README
+function renderLicenseSection(license) {
+  if (!license || license === 'None') return '';
+  return `## License
+This project is licensed under the [${license}](${renderLicenseLink(license)}) license.`;
+}
 
-// TODO: Create a function to generate markdown for README
+// Function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+${renderLicenseBadge(data.license)}
 
+## Description
+${data.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+${data.license !== 'None' ? '- [License](#license)' : ''}
+- [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## Contributing
+${data.contribution}
+
+## Tests
+${data.tests}
+
+${renderLicenseSection(data.license)}
+
+## Questions
+For any questions, please contact me at [${data.email}](mailto:${data.email}).
+GitHub: [${data.github}](https://github.com/${data.github})
 `;
 }
 
